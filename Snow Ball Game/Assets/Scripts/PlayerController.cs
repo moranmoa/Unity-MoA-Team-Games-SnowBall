1cc748f49c1ec793db99e217bf04a370274b65d8
Moran Abecassis <moranmoa@gmail.com>
2017-12-14 20:31:36 +0200

    animations

--- Snow Ball Game/Assets/Scripts/PlayerController.cs
+++ Snow Ball Game/Assets/Scripts/PlayerController.cs
@@ -19,9 +19,12 @@ public class PlayerController : MonoBehaviour {
 
 	private Rigidbody2D theRB;
 
+	private Animator anim;
+
 	// Use this for initialization
 	void Start () {
 		theRB = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -43,6 +46,17 @@ public class PlayerController : MonoBehaviour {
 		{
 			theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
 		}
+		if (theRB.velocity.x < 0) 
+		{
+			transform.localScale = new Vector3(-1,1,1);
+		}
+		else if(theRB.velocity.x > 0)
+		{
+			transform.localScale = new Vector3(1,1,1);
+		}
+		anim.SetFloat ("Speed", Mathf.Abs( theRB.velocity.x));
+		anim.SetBool ("Grounded", isGrounded);
+
 
 	}
 }
