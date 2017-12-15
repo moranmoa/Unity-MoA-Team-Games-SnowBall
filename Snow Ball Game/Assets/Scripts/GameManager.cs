45af0c092cfa5ab74c8efa022ec80bafbf3a03b5
Moran Abecassis <moranmoa@gmail.com>
2017-12-15 16:53:16 +0200

    lifebar works

--- Snow Ball Game/Assets/Scripts/GameManager.cs
+++ Snow Ball Game/Assets/Scripts/GameManager.cs
@@ -6,10 +6,14 @@ public class GameManager : MonoBehaviour {
 
 	public GameObject player1;
 	public GameObject player2;
+
 	public int p1Life;
 	public int p2Life;
 
 	public GameObject gameOver;
+
+	public GameObject[] p1Sticks;
+	public GameObject[] p2Sticks;
 
 	// Use this for initialization
 	void Start () {
@@ -32,9 +36,23 @@ public class GameManager : MonoBehaviour {
 
 	public void HurtP1(){
 		p1Life -= 1;
+		for (int i = 0; i < p1Sticks.Length; i++) {
+			if (p1Life > i) {
+				p1Sticks [i].SetActive (true);
+			} else {
+				p1Sticks [i].SetActive (false);
+			}
+		}
 	}
 
 	public void HurtP2(){
 		p2Life -= 1;
+		for (int i = 0; i < p2Sticks.Length; i++) {
+			if (p2Life > i) {
+				p2Sticks [i].SetActive (true);
+			} else {
+				p2Sticks [i].SetActive (false);
+			}
+		}
 	}
 }
