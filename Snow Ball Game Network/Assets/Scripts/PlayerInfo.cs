5ec492d8ead369bf549c450a8b6ba58fd84c28de
Moran Abecassis <moranmoa@gmail.com>
2017-12-22 22:27:34 +0200

    player name

--- Snow Ball Game Network/Assets/Scripts/PlayerInfo.cs
+++ Snow Ball Game Network/Assets/Scripts/PlayerInfo.cs
@@ -8,18 +8,48 @@ public class PlayerInfo : NetworkBehaviour {
 	public Color color;
 	[SyncVar]
 	public string name;
+	Rigidbody2D theRB;
+	Vector3 scale;
+	TextMesh thetext;
+
 	//public GameObject pname;
 	MeshRenderer rends;
 	// Use this for initialization
 	void Start () {
 		rends = GetComponentInChildren<MeshRenderer> ();
 		//for (int i = 0; i < rends.Length; i++)
-			rends .material.color = color;
-		GetComponentInChildren<TextMesh>().text = name;
+			rends.material.color = color;
+		thetext = GetComponentInChildren<TextMesh>();
+		thetext.text = name;
+		theRB = GetComponent<Rigidbody2D>();
+		scale = new Vector3(0.5f,0.5f,0.5f);
+		Cmdscale (scale);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (theRB.velocity.x < 0) 
+				{
+			scale = new Vector3(-0.5f,0.5f,0.5f);
+					Cmdscale (scale);
+				}
+				else if(theRB.velocity.x > 0)
+				{
+			scale = new Vector3(0.5f,0.5f,0.5f);
+					Cmdscale (scale);
+				}
+	}
+	[Command]
+	void Cmdscale(Vector3 scale)
+	{
+		Rpcscale(scale);
+	}
+
+	[ClientRpc]
+	void Rpcscale(Vector3 scale)
+	{
+		//if (isLocalPlayer)
+		//	return;
+		thetext.transform.localScale=scale;
 	}
 }
